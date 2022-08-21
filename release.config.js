1721c280b5297ad61b4db0b4dc6c7483d132a3e2
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-08-21 23:45:16 +0900

    xxo: :wrench: Fix config

--- release.config.js
+++ release.config.js
@@ -1,28 +1,29 @@
+const commitConfig = require("./lib/commit.config");
+
 module.exports = {
   branches: ["main"],
   plugins: [
     [
       "@semantic-release/commit-analyzer",
       {
-        config: "conventional-changelog-gitmoji-config",
-        releaseRules: [
-          {
-            scope: "x.x.x",
-            release: false,
-          },
-          {
-            scope: "x.x.o",
-            release: "patch",
-          },
-          {
-            scope: "x.o.x",
-            release: "minor",
-          },
-          {
-            scope: "o.x.x",
-            release: "major",
-          },
-        ],
+        preset: "conventionalcommits",
+        releaseRules: commitConfig.map((v) => ({
+          type: v.value,
+          release: v.release,
+        })),
+      },
+    ],
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "conventionalcommits",
+        presetConfig: {
+          types: commitConfig.map((v) => ({
+            type: v.value,
+            section: v.value,
+            hidden: false,
+          })),
+        },
       },
     ],
     "@semantic-release/github",
